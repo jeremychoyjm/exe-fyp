c6a011e85ca618e8477265e1302caddfa48e4ae5
jeremychoyjm <jeremychoyjm@gmail.com>
2023-03-22 16:19:51 +0800

    update

--- ultralytics/ultralytics/tracker/__init__.py
+++
@@ -1,4 +0,0 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
-
-from .track import register_tracker
-from .trackers import BOTSORT, BYTETracker
--- ultralytics/ultralytics/tracker/trackers/__init__.py
+++
@@ -1,4 +0,0 @@
-# Ultralytics YOLO 🚀, GPL-3.0 license
-
-from .bot_sort import BOTSORT
-from .byte_tracker import BYTETracker
